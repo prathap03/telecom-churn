4e58dd31fd2f19e3626c87e5c90c5275399c73c4
Joe Prathap P J <joeprathappj@gmail.com>
2023-10-08 20:33:35 +0530

    reconfig

--- next.config.js
+++ next.config.js
@@ -8,21 +8,21 @@ const nextConfig = {
           destination:
             process.env.NODE_ENV === "development"
               ? "http://127.0.0.1:8000/api/:path*"
-              : "/api/",
+              : "https://joeprathappj-lstmchurn.hf.space/api/",
         },
         {
           source: "/docs",
           destination:
             process.env.NODE_ENV === "development"
               ? "http://127.0.0.1:8000/docs"
-              : "/api/docs",
+              : "https://joeprathappj-lstmchurn.hf.space/api/docs",
         },
         {
           source: "/openapi.json",
           destination:
             process.env.NODE_ENV === "development"
               ? "http://127.0.0.1:8000/openapi.json"
-              : "/api/openapi.json",
+              : "https://joeprathappj-lstmchurn.hf.space/api/openapi.json",
         },
       ];
     },
