14cf9da3438250a4ff3d3ffa07d37df3484b1642
Joe Prathap P J <joeprathappj@gmail.com>
2023-10-08 20:35:45 +0530

    done

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
           destination:
             process.env.NODE_ENV === "development"
               ? "http://127.0.0.1:8000/api/:path*"
-              : "https://joeprathappj-lstmchurn.hf.space/api/",
+              : "https://joeprathappj-lstmchurn.hf.space/",
         },
         {
           source: "/docs",
